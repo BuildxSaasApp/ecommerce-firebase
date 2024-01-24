e5824dcc7098e647b59ba32c7cec6233bc41507c
BuildxSaas <156997599+BuildxSaasApp@users.noreply.github.com>
2024-01-24 02:51:44 -0300

    Update Config.js

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -7,12 +7,12 @@ import 'firebase/auth'
 //TROQUE PELO SEU FIREBASE CONFIG
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAs_fPHuGsNlTbOrYmEoat8kzTuTShnn7Y",
-  authDomain: "ecommerce-template-1d6ef.firebaseapp.com",
-  projectId: "ecommerce-template-1d6ef",
-  storageBucket: "ecommerce-template-1d6ef.appspot.com",
-  messagingSenderId: "615291422908",
-  appId: "1:615291422908:web:b726290e614e912304e8b9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
