b6e5e2dba8a8e2a287b52c9f2421a4980bfb35cc
BuildxSaas <156997599+BuildxSaasApp@users.noreply.github.com>
2024-01-24 02:48:55 -0300

    Update Config.js

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -4,16 +4,17 @@ import 'firebase/storage';
 import 'firebase/firestore';
 import 'firebase/auth'
 
+//TROQUE PELO SEU FIREBASE CONFIG
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDGmVBx5LAYJuIq_Ni1nZBenfcVUFX-SyY",
-    authDomain: "ecommerce-with-react-2ac06.firebaseapp.com",
-    databaseURL: "https://ecommerce-with-react-2ac06.firebaseio.com",
-    projectId: "ecommerce-with-react-2ac06",
-    storageBucket: "ecommerce-with-react-2ac06.appspot.com",
-    messagingSenderId: "690603499200",
-    appId: "1:690603499200:web:09860bc318b5b7fd74d725",
-    measurementId: "G-88N35MC51Q"
+  apiKey: "AIzaSyAs_fPHuGsNlTbOrYmEoat8kzTuTShnn7Y",
+  authDomain: "ecommerce-template-1d6ef.firebaseapp.com",
+  projectId: "ecommerce-template-1d6ef",
+  storageBucket: "ecommerce-template-1d6ef.appspot.com",
+  messagingSenderId: "615291422908",
+  appId: "1:615291422908:web:b726290e614e912304e8b9"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
